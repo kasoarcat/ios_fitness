c25ffda744f25e381c84173aef0f9a7b506d84e7
fantong <fankaihsiang11@gmail.com>
2021-05-26 00:18:14 +0800

    Finish the feature of notification

---
+++ ios_fitness/ios_fitness/Model/AlertOption.swift
@@ -0,0 +1,20 @@
+//
+//  AlertOption.swift
+//  ios_fitness
+//
+//  Created by 范桶 on 2021/5/25.
+//
+
+import Foundation
+
+struct AlertOption {
+    let id = UUID()
+    var type: AlertType = .none
+    var message: String
+}
+
+enum AlertType: String {
+    case error = "error"
+    case success = "success"
+    case none = ""
+}
